8441d5e36851aac963739364a68e0af9e7c531e6
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2018-09-18 23:38:09 -0300

    Change symbol.

--- src/regex.py
+++ src/regex.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 class Regex:
-    OPERATORS = ['|', '*', '+', '{', '}', '(', ')', 'ˆ']
+    OPERATORS = ['|', '*', '+', '{', '}', '(', ')', '^']
 
     def __init__(self, string):
         if self._validate(string):
