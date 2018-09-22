9c0ce8f4fe42899c8f93371e0edbee89baf46025
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2018-09-22 13:08:58 -0300

    Add initial methods for parsing the regex.

--- src/regex.py
+++ src/regex.py
@@ -4,15 +4,28 @@ class Regex:
     END = '#'
     OPERATORS = ['|', '*', '+', '.', '?']
 
-    def __init__(self, string):
-        if self._validate(string):
-            self.string = string
+    def __init__(self, input):
+        if self._validate(input):
+            self.input = input
         else:
             raise ValueError('Invalid input string.')
 
-    def _validate(self, string):
-        for c in string:
+    def _validate(self, input):
+        for c in input:
             if c not in self.OPERATORS:
                 return False
 
         return True
+
+    def _peek(self):
+        return self.input[0]
+
+    def _eat(self, char):
+        if _peek() != char
+            raise RuntimeError('Invalid input string.')
+
+        self.input = self.input[1:]
+
+    def _next(self):
+        _eat(_peek())
+        return char
