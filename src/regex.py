b53b748913976b0b61dccdf7782ff1df3dabdac7
Gabriel Chittolina <gabrielchittolina1@gmail.com>
2018-09-22 18:48:15 -0300

    Add some more functions to regex parsing.

--- src/regex.py
+++ src/regex.py
@@ -7,13 +7,28 @@ GLC:
 <term>      ::=  { <factor> }
 <factor>    ::= <base> { '*' }
                 <base> { '?' }
+                <base> { '.' }
 <base>      ::= <char>
                 '(' <regex> ')'
-
 '''
-class Regex:
-    END = '#'
-    OPERATORS = ['|', '*', '.', '?']
+
+
+class Node:
+    count = 0
+
+    def __init__(self, symbol, left, right):
+        self.symbol = symbol
+        self.left = left
+        self.right = right
+        self.index = Node.count
+        Node.count += 1
+
+
+END = '#'
+OPERATORS = ['|', '*', '.', '?']
+ALPHABET = ['a', 'b']
+
+class RegexParser:
 
     def __init__(self, input):
         if self._validate(input):
@@ -28,7 +43,7 @@ class Regex:
         return self.input[0]
 
     def _eat(self, char):
-        if _peek() != char
+        if self._peek() != char
             raise RuntimeError('Invalid input string.')
 
         self.input = self.input[1:]
@@ -37,8 +52,40 @@ class Regex:
         return len(self.input) > 0
 
     def _next(self):
-        char = _peek()
-        _eat(char)
+        char = self._peek()
+        self._eat(char)
         return char
 
     def _regex(self):
+        return True
+
+    def _base(self):
+        char = self._peek()
+
+        if char == '(':
+            self._eat('(')
+            regex = self._regex()
+            self._eat(')')
+            return regex
+        elif char in ALPHABET:
+            node = Node(char, None, None)
+            return node
+        else:
+            raise RuntimeError('Invalid input string.')
+
+    def _factor(self):
+        base = self._base()
+        char = self._peek()
+
+        while self._more() and self._peek() in ['?', '*']:
+            self._eat(self._peek())
+
+        return base
+
+    def _term(self):
+        factor = self._factor()
+
+        if self._peek() in ['|', ')']:
+            node = Node('.', factor, self._term())
+
+        return factor
