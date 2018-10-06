472c7ba66513145648714f2c811a26fa0586b0ab
Filipe Borba <filipe.borba@grad.ufsc.br>
2018-10-06 19:47:58 +0000

    Convert grammar to automaton

--- src/grammar.py
+++ src/grammar.py
@@ -1,5 +1,7 @@
 import json
 
+from automata import Automata, Utils
+
 class Grammar(object):
 
     def __init__(self, productions, initial_symbol):
@@ -12,6 +14,14 @@ class Grammar(object):
         self._productions = set(productions)
         self._nonterminals = self._getNonterminals()
         self._terminals = self._getTerminals()
+
+    def to_automaton(self):
+        transitions = self._makeTransitions()
+        final_states = set(FINAL_STATE)
+        q0 = self._initial_symbol
+        states = self._nonterminals | final_states
+        alphabet = self._terminals
+        return Automata(alphabet, states, q0, final_states, transitions)
 
     def save_json(self, filename):
         data = {
@@ -34,6 +44,24 @@ class Grammar(object):
         for production in self._productions:
             terminals.add(production[1])
         return terminals
+
+    def _makeTransitions(self):
+        transitions = dict()
+        for production in self._productions:
+            input = Utils.TRANSITION(production[0], production[1])
+            output = self._getNextState(production)
+            self._includeTransition(transitions, input, output)
+        return transitions
+
+    def _getNextState(self, production):
+        if len(production) == 3:
+            return production[2]
+        return FINAL_STATE
+
+    def _includeTransition(self, transitions, input, output):
+        if not input in transitions.keys():
+            transitions[input] = list()
+        transitions[input] = output
 
     def read_from_json(filename):
         with open(filename + '.json', 'r') as read_file:
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ class Utils:
 
     TRANSITION = namedtuple('Transition', ['state', 'char'])
     EPSILON = '&'
+    NEW_FINAL_STATE = '$' # used in grammar to automaton conversion
 
     def __init__(self):
         pass
